5dba25d5082c515c54780d7d9d634665a5b94df1
Abib Mishael P <abibmishael1@gmail.com>
2021-05-26 04:16:49 +0530

    Backend Code changes with intial commit

---
+++ routes/users.js
@@ -0,0 +1,60 @@
+let async = require('async');
+var express = require('express');
+var router = express.Router();
+let serviceUsers = require('../service/users');
+
+/* GET home page. */
+
+//Read
+router.get('/', async (req, res) => {
+  let result;
+  try {
+    result = await serviceUsers.dashboard(req);
+    console.log("Result", result);
+  } catch (e) {
+    console.log("Error", e);
+  } finally {
+    res.render('index', { title: result });
+  }
+});
+
+//Create
+router.post('/', async (req, res) => {
+  let result;
+  try {
+    result = await serviceUsers.create(req);
+    console.log("Result", result);
+  } catch (e) {
+    console.log("Error", e);
+  } finally {
+    res.render('index', { title: result });
+  }
+});
+
+//Edit
+router.put('/', async (req, res) => {
+  let result;
+  try {
+    result = await serviceUsers.edit(req);
+    console.log("Result", result);
+  } catch (e) {
+    console.log("Error", e);
+  } finally {
+    res.render('index', { title: result });
+  }
+});
+
+//Delete
+router.delete('/', async (req, res) => {
+  let result;
+  try {
+    result = await serviceUsers.delete(req);
+    console.log("Result", result);
+  } catch (e) {
+    console.log("Error", e);
+  } finally {
+    res.render('index', { title: result });
+  }
+});
+
+module.exports = router;
---
+++ service/users.js
@@ -0,0 +1,85 @@
+//Getting Details
+exports.dashboard = async (req) => {
+  let dbclient;
+  const collection = req.app.locals.collection;
+  try {
+    dbclient = await collection.find({}).toArray().
+      then(response => {
+        return response;
+      }).catch(e => {
+        throw e;
+      });
+    console.log("result", dbclient);
+    return dbclient
+  } catch (error) {
+    return error;
+  }
+}
+
+//Creating Function
+exports.create = async (req) => {
+  let dbclient;
+  const collection = req.app.locals.collection;
+  try {
+    dbclient = await collection.insert({
+      "name": req.body.name,
+      "img": req.body.img,
+      "summary": req.body.summary
+    }).then(response => {
+      return response;
+    }).catch(e => {
+      throw e;
+    });
+    console.log("result", dbclient);
+    return dbclient
+  } catch (error) {
+    return error;
+  }
+}
+
+//Editing Function
+exports.edit = async (req) => {
+  let dbclient;
+  const collection = req.app.locals.collection;
+  try {
+    let query = req.body._id;
+    let updateQuery = {
+      $set: {
+        "name": req.body.name,
+        "img": req.body.img,
+        "summary": req.body.summary,
+      }
+    }
+    console.log("query", req.body._id, JSON.stringify(query), JSON.stringify(updateQuery));
+    dbclient = await collection.updateMany(
+      query,
+      updateQuery).catch(e => {
+        throw e;
+      });
+    console.log("result", dbclient);
+    return dbclient
+  } catch (error) {
+    return error;
+  }
+}
+
+//Deleting Function
+exports.delete = async (req) => {
+  let dbclient;
+  const collection = req.app.locals.collection;
+  try {
+    let query = "ObjectId(" + JSON.stringify(req.body.id) + ")";
+    console.log("query", query);
+    dbclient = await collection.deleteOne({ "_id": query });
+    console.log("result", dbclient.deletedCount);
+    if (dbclient.deletedCount > 0) {
+      return "Deleted Successfully";
+    } else {
+      return "Not Found";
+    }
+  } catch (error) {
+    return error;
+  }
+}
+
+
